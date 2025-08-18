2de5f1542ac9a1b944725b483e4aa831f5560abc
Laurence298 <laurencemunoko@gmail.com>
2025-08-18 10:20:10 -0600

    final push 4

--- app/models/service_detail.rb
+++ app/models/service_detail.rb
@@ -1,4 +1,4 @@
 class ServiceDetail < ApplicationRecord
   belongs_to :service
-  belongs_to :requested_service, optional: true  # optional if you want to link it later
+  belongs_to :requested_service, optional: true
 end
